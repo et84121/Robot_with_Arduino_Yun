a6f84ee13dee20cf477aad15b4b7e60e48b16fe2
et84121 <f411024@gmail.com>
2015-11-24 20:54:40 +0800

    add stand up and sit down
    還沒有測試

--- Robot_with_Arduino_Yun.ino
+++ Robot_with_Arduino_Yun.ino
@@ -1,22 +1,22 @@
 /**************************************************************
- * Blynk is a platform with iOS and Android apps to control
- * Arduino, Raspberry Pi and the likes over the Internet.
- * You can easily build graphic interfaces for all your
- * projects by simply dragging and dropping widgets.
- *
- *   Downloads, docs, tutorials: http://www.blynk.cc
- *   Blynk community:            http://community.blynk.cc
- *   Social networks:            http://www.fb.com/blynkapp
- *                               http://twitter.com/blynk_app
- *
- * Blynk library is licensed under MIT license
- * This example code is in public domain.
- *
+   Blynk is a platform with iOS and Android apps to control
+   Arduino, Raspberry Pi and the likes over the Internet.
+   You can easily build graphic interfaces for all your
+   projects by simply dragging and dropping widgets.
+
+     Downloads, docs, tutorials: http://www.blynk.cc
+     Blynk community:            http://community.blynk.cc
+     Social networks:            http://www.fb.com/blynkapp
+                                 http://twitter.com/blynk_app
+
+   Blynk library is licensed under MIT license
+   This example code is in public domain.
+
  **************************************************************
- * This example shows how to use Arduino Yun Bridge
- * to connect your project to Blynk.
- * Feel free to apply it to any other example. It's simple!
- *
+   This example shows how to use Arduino Yun Bridge
+   to connect your project to Blynk.
+   Feel free to apply it to any other example. It's simple!
+
  **************************************************************/
 
 #define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
@@ -38,19 +38,28 @@ void setup()
   //Blynk.begin(auth, "server.org", 8442);
   //Blynk.begin(auth, server_ip, port);
 
-  for(int a=2;a<=9;a++){
-    myservo[a-2].attach(a);
+  for (int a = 2; a <= 9; a++) {
+    myservo[a - 2].attach(a);
   }
 }
 
 
 //make robot stand up
-BLYNK_WRITE(V1) 
+BLYNK_WRITE(V1)
 {
   int pinData = param.asInt();
-  if(pinData == 1){
-    for(int a=0;a<=8;a++){
-    myservo[a].write(90);
+  //if value is 1 , make robot stand up. Or make robot sit down
+  if (pinData == 1) {
+    for (int a = 0; a <= 8; a++) {
+      myservo[a].write(90);
+    }
+  }
+  else {
+    for (int a = 0; a <= 7; a += 2) {
+      myservo[a].write(0);
+    }
+    for (int a = 1; a <= 7; a += 2) {
+      myservo[a].write(90);
     }
   }
 }
@@ -59,4 +68,3 @@ void loop()
 {
   Blynk.run();
 }
-
